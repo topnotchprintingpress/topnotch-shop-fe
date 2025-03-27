4de51ef2ead380c9539dd60c182131006bacd35c
geofrey <devongeoffreymaina@gmail.com>
2025-03-27 10:15:54 +0300

    Changed callback redirect

--- components/buttons/Googlebtn.tsx
+++ components/buttons/Googlebtn.tsx
@@ -7,7 +7,11 @@ export default function GoogleSignInButton() {
   return (
     <button
       className="bg-white flex justify-center text-xs md:text-base items-center gap-4 border border-zinc-300 hover:bg-[#3502033b] hover:text-[#350203] px-4 md:px-8 py-2 rounded-2xl w-full text-zinc-700"
-      onClick={() => signIn("google", { callbackUrl: window.location.origin })}
+      onClick={() =>
+        signIn("google", {
+          callbackUrl: "https://www.topnotchprintingpress.com",
+        })
+      }
     >
       <span className="text-red-700 mr-2">
         <FcGoogle size={30} />
