fc84f7c1bd50d0ec20e4ce5f8b49c79ca0c4f483
geofrey <devongeoffreymaina@gmail.com>
2025-04-08 01:50:53 +0300

    Signout config

--- components/buttons/SignOut.tsx
+++ components/buttons/SignOut.tsx
@@ -1,15 +1,22 @@
 import { signOut } from "next-auth/react";
 import { RiLogoutCircleRLine } from "react-icons/ri";
+import { useRouter } from "next/router";
 
 function SignOut() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut({ redirect: false });
+
+    router.push("/signin");
+  };
+
   return (
     <button
       className="text-[#350203] flex gap-3 justify-center items-center"
-      onClick={() => signOut({ callbackUrl: "/signin" })}
+      onClick={handleSignOut}
     >
-      <span>
-        <RiLogoutCircleRLine size={20} />
-      </span>
+      <RiLogoutCircleRLine size={20} />
       Sign Out
     </button>
   );
